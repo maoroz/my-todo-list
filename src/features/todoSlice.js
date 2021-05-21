a1b14288371eba69ab4334e9453accab0dc8aa06
maoroz <angelesoroz@hotmail.com>
2021-05-21 00:09:08 -0300

    Update todoSlice.js

--- src/features/todoSlice.js
+++ src/features/todoSlice.js
@@ -1,12 +1,13 @@
-//import axios from 'axios';
-import { client } from '../api/client';
+import axios from 'axios';
 
 const initialState = []
+let howMuchData = 5;
 
 export default function todosReducer(state = initialState, action) {
   switch (action.type) {
     case 'todos/todoAdded': {
-      return [...state, action.payload]
+      howMuchData = howMuchData + 1;
+      return [...state, action.payload];
     }
     case 'todos/todoToggled': {
       return state.map((todo) => {
@@ -20,29 +21,12 @@ export default function todosReducer(state = initialState, action) {
         }
       })
     }
-    case 'todos/colorSelected': {
-      const { color, todoId } = action.payload
-      return state.map((todo) => {
-        if (todo.id !== todoId) {
-          return todo
-        }
-
-        return {
-          ...todo,
-          color,
-        }
-      })
-    }
     case 'todos/todoDeleted': {
-      return state.filter((todo) => todo.id !== action.payload)
+      howMuchData = howMuchData - 1;
+      return state.filter((todo) => todo.id !== action.payload);;
     }
-    case 'todos/allCompleted': {
-      return state.map((todo) => {
-        return { ...todo, completed: true }
-      })
-    }
-    case 'todos/completedCleared': {
-      return state.filter((todo) => !todo.completed)
+    case 'todos/deleteAll': {
+      return initialState;
     }
     case 'todos/todosLoaded': {
       return action.payload ? action.payload : state
@@ -53,15 +37,7 @@ export default function todosReducer(state = initialState, action) {
 };
 
 export async function fetchTodos(dispatch, getState) {
-  const response = await client.get('https://jsonplaceholder.typicode.com/todos');
-  const data = response.slice(0, 10);
+  const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
+  const data = response.data.slice(0, howMuchData);
   dispatch({ type: 'todos/todosLoaded', payload: data });
-};
-
-export function saveNewTodo(text) {
-  return async function saveNewTodoThunk(dispatch, getState) {
-    const initialTodo = { text }
-    const response = await client.post('https://jsonplaceholder.typicode.com/todos', { todo: initialTodo })
-    dispatch({ type: 'todos/todoAdded', payload: response.todo })
-  }
 };
