88d78496dbbe8dc3cca04be837fd9c41527bf88f
maoroz <angelesoroz@hotmail.com>
2021-05-21 00:09:46 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import App from "./App";
 
 import { Provider } from 'react-redux';
 import store from './app/store';
+import { fetchTodos } from './features/todoSlice';
+
+store.dispatch(fetchTodos);
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
