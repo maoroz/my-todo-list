da49aff4d5e2cb2fac480b783b9b1fd9ed55f84c
maoroz <angelesoroz@hotmail.com>
2021-05-21 00:07:11 -0300

    Update todoList.js

--- src/components/todoList/todoList.js
+++ src/components/todoList/todoList.js
@@ -1,86 +1,71 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { useSelector, shallowEqual, useDispatch } from 'react-redux'
 import TodoListItem from './todoListItem'
 import Button from '@material-ui/core/Button';
 import AddIcon from '@material-ui/icons/Add';
 import DeleteIcon from '@material-ui/icons/Delete';
 import List from '@material-ui/core/List';
+import Toolbar from '@material-ui/core/Toolbar';
+import ListItemText from '@material-ui/core/ListItemText';
 import './todoList.css';
-
-import { fetchTodos } from '../../features/todoSlice';
 
 const useStyles = makeStyles((theme) => ({
 	root: {
 	  width: '100%',
 	  maxWidth: 360,
-	  //backgroundColor: theme.palette.background.paper,
+	},
+	clearTodo: {
+		margin: 20,
 	},
 }));
 
-const selectTodoIds = (state) => state.todos.map((todo) => todo.id)
-// import { useSelector, useDispatch } from 'react-redux';
-// import { selectTodo, addTodo, deleteTodo, fetchTodos } from '../../features/todoSlice';
-
-const ToDoList = () => {
+export default function ToDoList ({
+	text,
+	todoIdsArray,
+	handleChange,
+	handleSubmit,
+	deleteAll,
+}) {
 	const classes = useStyles();
-	const dispatch = useDispatch();
-	dispatch(fetchTodos);
-
-	const todoIds = useSelector(selectTodoIds, shallowEqual);
-	
-	const [item, setItem] = useState("");
-	const [newItem, setNewItem] = useState([]);
-	
-	// const dispatch = useDispatch();
-
-	const firstEvent = (event) => {
-		setItem(event.target.value);
-	}
-	
-	const secondEvent = () => {
-		
-		//dispatch(addTodo(item));
-		
-		setItem("");
-		
-	}
-	
-	const thirdEvent = () => {
-		//dispatch(deleteTodo());
-	}
 
 	return(
 		<div>
 			<br />
 			<br />
 			<div className="childOne">
-				<input type="text" value={item} placeholder="I have to.." onChange={firstEvent} />
-				<Button className="AddBtn" onClick={secondEvent}>
-					<AddIcon />
-				</Button>
+				<Toolbar variant="dense">
+					<input
+						type="text"
+						className="new-todo"
+						placeholder="I have to.."
+						value={text}
+						onChange={handleChange}
+					/>
+					<Button variant="contained" color="secondary" onClick={handleSubmit}>
+						<AddIcon />
+					</Button>
+				</Toolbar>
 				<br />
 				<br />
-				<List className={classes.root}>
-					{/*<ul className="todo-list">*/}
+				{
+					todoIdsArray.length > 0 ? 
+					<List className={classes.root}>
 						{
-							todoIds.map((todoId) => {
+							todoIdsArray.map((todoId) => {
 								return <TodoListItem key={todoId} id={todoId} />
 							})
 						}
-					{/*</ul>*/}
-				</List>
+					</List>
+					:	<ListItemText id={`checkbox-list-label-0`} primary={"Nothing to do!"} className={classes.clearTodo} />
+				}
 			</div>
 			<br />
 			<br />
 			<div className="childTwo">
-				<Button className="delBtn" onClick={thirdEvent}>
+				<Button className="delBtn" onClick={deleteAll}>
 					<DeleteIcon />Delete All
 				</Button>
 			</div>
 		</div>
 	);
 }
-
-
-export default ToDoList;
